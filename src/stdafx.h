83cac1854be9090d32fcb8d50533847499f49fac
HADMARINE <tsx@kakao.com>
2020-09-25 20:35:45 +0900

    Update stdafx.h

--- src/stdafx.h
+++ src/stdafx.h
@@ -1,16 +1,14 @@
 #pragma once
 
-//#define WIN32_LEAN_AMD_MEAN
+#define WIN32_LEAN_AMD_MEAN
 
-#include "targetver.h"
-
-
-#include <stdio.h>
-#include <memory.h>
-#include <iostream>
-#include <functional>
-#include <string>
-#include <cstdlib>
-#include "global_preferences.h"
 #include "CLogger.hpp"
+#include "GlobalPrefences.hpp"
+#include "targetver.h"
 #include <algorithm>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <memory.h>
+#include <stdio.h>
+#include <string>
