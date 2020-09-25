f29534805040458c7daceed27fc68c2a335ca889
HADMARINE <tsx@kakao.com>
2020-09-25 20:36:02 +0900

    Update socketManager

--- src/SocketManager.hpp
+++ src/SocketManager.hpp
@@ -9,21 +9,79 @@
 #include <openssl/bio.h>
 #include <openssl/buffer.h>
 #include "service_error.hpp"
+#include "Parser.hpp"
 
-using namespace restbed;
 
 namespace Socket{
+  using namespace std;
+  using namespace restbed;
+  
 	multimap<string, string>build_websocket_handshake_response_headers(const shared_ptr<const Request>&);
-	string base64_encode(const unsigned char*, int);
-	
+  string base64_encode(const unsigned char*, int);
+  
+//  enum class SocketType {
+//    TEXT_FRAME,
+//
+//  };
 
-	class SocketCollector {
+  class REQUEST {
+  public:
+    REQUEST(const shared_ptr<WebSocket>&, const shared_ptr<WebSocketMessage>&);
+    Json::Value getJson();
+    std::string getString();
+    std::string getId();
+    
+    shared_ptr<WebSocket> getRawSource() {
+      return this->source;
+    }
+    
+    shared_ptr<WebSocketMessage> getRawMessage() {
+      return this->message;
+    }
+    
+  private:
+    shared_ptr<WebSocket> source;
+    shared_ptr<WebSocketMessage> message;
+  };
+  
+  class RESPONSE {
+  public:
+    RESPONSE(const shared_ptr<WebSocket>&, const shared_ptr<WebSocketMessage>&);
+    void broadcast(const std::string&);
+    void emit_str(std::string);
+    void emit_json(Json::Value);
+    void emit_to_str(std::string, std::string);
+    void emit_to_json(std::string, Json::Value);
+    
+    shared_ptr<WebSocket> getRawSource() {
+      return this->source;
+    }
+  
+    shared_ptr<WebSocketMessage> getRawMessage() {
+      return this->message;
+    }
+
+  private:
+    shared_ptr<WebSocket> source;
+    shared_ptr<WebSocketMessage> message;
+  };
+  
+  class SocketManager {
 	public:
-//		SocketCollector();
-//		~SocketCollector();
+    explicit SocketManager(string, const function<void(REQUEST, RESPONSE)>&);
+		~SocketManager() = default;
 		static map<string, shared_ptr<WebSocket>> sockets;
+    static map<string, const function<void(REQUEST, RESPONSE)>> socketEvents;
 		static bool Initialize(Service&);
-	private:
-		string event;
-	};
+    static void messageHandler(const shared_ptr<WebSocket>&, const shared_ptr<WebSocketMessage>&);
+    static void closeHandler(const shared_ptr<WebSocket>&);
+    static void errorHandler(const shared_ptr<WebSocket>&, error_code);
+//    string getEvent() {
+//        return this->event;
+//    };
+//  private:
+//		string event;
+  };
+  
+  function<void(shared_ptr<Session>)> WRAP_FUNC(function<void(REQUEST, RESPONSE)>);
 }
