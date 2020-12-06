b54163906025bf7adf5ba7f4aa8ad3061bd247e9
HADMARINE <tsx@kakao.com>
2020-12-06 23:27:27 +0900

    Update globalPreferences

--- src/GlobalPrefences.hpp
+++ src/GlobalPrefences.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
-namespace GlobalPrefences {
+namespace GlobalPreferences {
   constexpr const int PORT = 61000;
-  constexpr const int REST_WORKER_LIMIT = 1;
+  constexpr const int REST_WORKER_LIMIT = 5;
   constexpr const char* APPLICATION_NAME = "CPP_BACKEND_BOILERPLATE";
   constexpr const char* WEBSOCKET_PATH = "/ws";
-  constexpr const bool ISDEBUGMODE = true;
-  constexpr const char* WEBSOCKET_KEY = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+  constexpr const bool DEBUG = true;  
+  constexpr const char* JWT_TOKEN_KEY = "TOKEN_TEST_TEST_TOKEN";
+  constexpr const char* JWT_ISSUER = "www.hadmarine.com";
 }
