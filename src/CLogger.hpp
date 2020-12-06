775e371033f93a975fcaf36864129ae3d525df9c
HADMARINE <tsx@kakao.com>
2020-12-06 23:29:07 +0900

    Update CLogger header

--- src/CLogger.hpp
+++ src/CLogger.hpp
@@ -1,28 +1,30 @@
 #pragma once
 
-#include <ctime>
+#include "pch.hpp"
 #include <cstdarg>
+#include <ctime>
 #include <string>
-#include "stdafx.h"
 
 #if defined(_WIN32) || defined(_WIN64)
 #include <Windows.h>
 #endif
 
-enum COLOR {
-	BLACK = 0, BLUE, GREEN, CYAN, RED, MAGENTA, BROWN,
-	LIGHTGRAY, DARKGRAY, LIGHTBLUE, LIGHTGREEN, LIGHTCYAN,
-	LIGHTRED, LIGHTMAGENTA, YELLOW, WHITE
-};
+
 
 class CLogger {
 public:
-	static bool isUsing;
-	static void RawLogger(const char *, ...);
-	static void Info(const char *, ...);
-	static void ErrorWithDialog(const char *, ...);
-	static void Error(const char *, ...);
-	static void Debug(const char *, ...);
-	static void ClearWindow();
-	static std::string getTimeNow();
+  enum class COLOR {
+    BLACK = 0, BLUE, GREEN, CYAN, RED, MAGENTA, BROWN,
+    LIGHTGRAY, DARKGRAY, LIGHTBLUE, LIGHTGREEN, LIGHTCYAN,
+    LIGHTRED, LIGHTMAGENTA, YELLOW, WHITE
+  };
+  static bool isUsing;
+  static void RawLogger(const char *, ...);
+  static void Info(const char *, ...);
+  static void ErrorWithDialog(const char *, ...);
+  static void Error(const char *, ...);
+  static void Debug(const char *, ...);
+  static void Success(const char *, ...);
+  static void ClearWindow();
+  static std::string getTimeNow();
 };
