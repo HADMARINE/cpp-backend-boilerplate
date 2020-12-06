a5badad932514d91c1dc16dd05f4bfac00bdb2c8
HADMARINE <tsx@kakao.com>
2020-12-06 23:03:49 +0900

    Update precompiled headers

--- src/stdafx.h
+++ src/stdafx.h
@@ -1,14 +1,24 @@
-#pragma once
+//
+// Created by HADMARINE on 2020/09/20.
+//
 
-#define WIN32_LEAN_AMD_MEAN
-
+#include "Assets.hpp"
 #include "CLogger.hpp"
 #include "GlobalPrefences.hpp"
-#include "targetver.h"
 #include <algorithm>
+#include <cstdarg>
 #include <cstdlib>
 #include <functional>
 #include <iostream>
-#include <memory.h>
-#include <stdio.h>
+#include <cstdio>
 #include <string>
+#include <vector>
+#include <list>
+#include <queue>
+#include <stdexcept>
+#include <set>
+#include <map>
+
+#if defined(_WIN32) || defined(_WIN64)
+#define NOMINMAX
+#endif
